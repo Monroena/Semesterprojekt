3cb5581cfc5bf9a011d25a07630a7f005dd42a5f
Schasti <imamshah19@yahoo.com>
2021-11-26 09:00:12 +0100

    hash

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -7,12 +7,16 @@ import model.LoginData;
 import model.User;
 
 import javax.ws.rs.WebApplicationException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.sql.SQLException;
 
 public class LoginController {
 
-    private LoginController() {
-    }
+    private LoginController() {}
 
     static private final LoginController loginControllerOBJ = new LoginController();
 
@@ -20,15 +24,10 @@ public class LoginController {
         return loginControllerOBJ;
     }
 
-    public boolean loginVal(String brugerliste, String pass){
-        if (brugerliste.length() > 1) {
-            String[] opdelt = brugerliste.split("A");
-            if (opdelt[1].equals(pass)) {
-                return true;
-            }
-        }
-        return false;
-    }
+
+
+
+
 
     public String doLogin(LoginData loginData) {
         try {
@@ -44,6 +43,39 @@ public class LoginController {
             e.printStackTrace();
         }
             throw new WebApplicationException("fejl", 401);
+    }
+
+
+    public boolean loginVal(String brugerliste, String pass){
+        if (brugerliste.length() > 1) {
+            String[] opdelt = brugerliste.split("B");
+            int salt = Integer.parseInt(opdelt[2]);
+            String hashcheck = generateHash(pass,salt);
+            if (opdelt[1].equals(hashcheck)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String generateHash(String pass, int salt){
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            pass+=String.valueOf(salt);
+            md5.update(StandardCharsets.UTF_8.encode(pass));
+            return String.format("%032x", new BigInteger(1, md5.digest()));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return pass;
+    }
+
+    public static int getSalt(){
+        byte[] salt = new byte[20];
+        SecureRandom sr = new SecureRandom();
+        sr.nextBytes(salt);
+        int saltint=salt[4];
+        return saltint;
     }
 }
 
